d4f8c8946aeabbb96568c106e4947be7339b5043
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-15 12:26:13 -0300

    MenuMobile Refatorado

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -9,22 +9,42 @@ export default class MenuMobile {
     this.menuButton = document.querySelector(menuButton);
     this.menuList = document.querySelector(menuList);
     this.eventos = eventos || ['click', 'touchstart'];
+
+    this.openMenu = this.openMenu.bind(this);
   }
 
   openMenu() {
     this.menuList.classList.add('active');
     this.menuButton.classList.add('active');
+    this.addAccessibility();
     outsideClick(this.menuList, this.eventos, () => {
       this.menuList.classList.remove('active');
       this.menuButton.classList.remove('active');
+      this.addAccessibility();
     });
   }
 
+  addAccessibility() {
+    const btn = this.menuButton;
+    const active = btn.classList.contains('active');
+    btn.setAttribute('aria-expanded', active);
+    if (active) {
+      btn.setAttribute('aria-label', 'Fechar menu');
+    } else {
+      btn.setAttribute('aria-label', 'Abrir menu');
+    }
+  }
+
+  addMobileEvents() {
+    this.eventos.forEach((evento) =>
+      this.menuButton.addEventListener(evento, this.openMenu)
+    );
+  }
+
   init() {
-    if (this.menuButton) {
-      this.eventos.forEach((evento) =>
-        this.menuButton.addEventListener(evento, this.openMenu)
-      );
+    if (this.menuButton && this.menuList) {
+      this.addMobileEvents();
+      this.addAccessibility();
     }
   }
 }

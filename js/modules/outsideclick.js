0398705120cade75747ebeddc561dc19f1c5f519
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-16 17:56:16 -0300

    Documentação outsideclick

--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -1,6 +1,16 @@
+/**
+ * Função para detectar clique fora de um elemento.
+ * @param {HTMLElement} element - O elemento para o qual se deseja detectar o clique fora.
+ * @param {Array<string>} events - Array de eventos que acionam a verificação de clique fora.
+ * @param {Function} callback - Função de retorno chamada quando ocorre um clique fora do elemento.
+ */
 export default function outsideClick(element, events, callback) {
   const html = document.documentElement;
   const outside = 'data-outside';
+  /**
+   * Manipula o evento de clique fora do elemento.
+   * @param {Event} event - O objeto de evento.
+   */
   function handleOutsideClick(event) {
     if (!element.contains(event.target)) {
       element.removeAttribute(outside);
@@ -10,11 +20,13 @@ export default function outsideClick(element, events, callback) {
       callback();
     }
   }
-
+  // Verifica se o elemento não possui o atributo "data-outside" (tipo: boolean).
   if (!element.hasAttribute(outside)) {
+    // Adiciona os manipuladores de evento para cada evento fornecido.
     events.forEach((userEvent) => {
       setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
     });
+    // Define o atributo "data-outside" no elemento para marcar que o evento foi adicionado.
     element.setAttribute(outside, '');
   }
 }

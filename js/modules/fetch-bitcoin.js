74eabfc12c128b33a3196bffeda709c629f52fc0
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-16 17:15:17 -0300

    Documentação fetchBitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,4 +1,9 @@
 /* eslint-disable arrow-parens */
+/**
+ * Função fetchBitcoin para buscar e exibir o preço do Bitcoin.
+ * @param {string} url - O URL para buscar os dados do Bitcoin.
+ * @param {string} target - O seletor CSS para o elemento onde o preço será exibido.
+ */
 export default function fetchBitcoin(url, target) {
   fetch(url)
     .then((response) => response.json())

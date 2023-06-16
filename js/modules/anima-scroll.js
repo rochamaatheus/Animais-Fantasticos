ebb946f9a87fa906f874e4aad8521710225c5147
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-16 17:02:00 -0300

    Documentação AnimaScroll

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -1,13 +1,37 @@
 import debounce from './debounce.js';
 
+/**
+ * Classe AnimaScroll para animar elementos conforme a rolagem da página.
+ * @class
+ * @export
+ */
 export default class AnimaScroll {
+  /**
+   * Construtor da classe AnimaScroll.
+   * @constructor
+   * @param {string} sections - Seletor CSS para as seções a serem animadas.
+   */
   constructor(sections) {
+    /**
+     * Lista de elementos de seções.
+     * @type {NodeList}
+     */
     this.sections = document.querySelectorAll(sections);
+    /**
+     * Metade da altura da janela para calcular a visibilidade da seção.
+     * @type {number}
+     */
     this.windowMetade = window.innerHeight * 0.6;
-
+    /**
+     * Função debounce para melhorar o desempenho ao lidar com eventos de rolagem.
+     * É necessário fazer o bind para manter o contexto da classe.
+     */
     this.animaScroll = debounce(this.animaScroll.bind(this), 50);
   }
 
+  /**
+   * Função para animar as seções com base na posição de rolagem.
+   */
   animaScroll() {
     this.sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
@@ -20,10 +44,18 @@ export default class AnimaScroll {
     });
   }
 
+  /**
+   * Remove o evento de rolagem.
+   */
   stop() {
     window.removeEventListener('scroll', this.animaScroll);
   }
 
+  /**
+   * Inicializa o componente AnimaScroll.
+   * Verifica se existem seções definidas e adiciona o evento de rolagem.
+   * @returns {AnimaScroll} - A instância do objeto AnimaScroll.
+   */
   init() {
     if (this.sections.length) {
       this.animaScroll();
